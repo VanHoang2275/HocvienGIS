c81590775f2b29a6c47e3031e02a06a6ea8c3d2a
VanHoang2275 <vanhoang2275@gmail.com>
2026-04-21 15:26:20 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-Simport tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
     base: '/HocvienGIS/',
     plugins: [react(), tailwindcss()],
